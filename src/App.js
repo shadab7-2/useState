726c168ebe8bd06bd6fa007a741c515a23477e24
shadab <sahdabansari972@gmail.com>
2023-04-27 10:49:29 +0530

    adding here nameupdater assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [start , end] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  const handleClick = (event) => {
+
+    setHeading(start);
+  }
+
+  const handleChange = (event) => {
+    end(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Hi my name is: {headingText}!</h1>
+      <form>
+        <input className='inputTah' placeholder='write name'  onChange={handleChange} value={start}/>
+        <button type="submit" onClick={handleClick} >Submit</button>
+      </form>
     </div>
   );
 }
